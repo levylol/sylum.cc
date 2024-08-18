df1fabae1c7d06a4a0312f3d1a32e628cb5515bb
levylol <sugmaballs1600@gmail.com>
2024-08-17 21:32:23 -0400

    Delete assets directory

--- assets/loader.js
+++
@@ -1,6 +0,0 @@
-let x = 1;
-let int = setInterval(() => {
-    if (x == document.querySelector(".loader").offsetWidth) x = 0
-    x += 1
-    document.querySelectorAll(".loader span")[0].style.marginLeft = x + "px"
-}, 10);
